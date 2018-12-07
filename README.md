e34a1db1489280990d84c433b5e9a1fd919d2d1c
yanqiangmiffy <1185918903@qq.com>
2018-12-07 13:58:38 +0800

    词向量特征

--- README.md
+++ README.md
@@ -25,3 +25,37 @@ Tfidf
 - tfidf_sum tfidf相加之后的和
 - tfidf_mean tfidf的平均值
 - tfidf_len tfidf向量的长度
+
+simhash
+
+- simhash_tokenize_distance
+- simhash_word_2gram_distance
+- simhash_word_3gram_distance
+- simhash_char_3gram_distance
+
+embedding 特征
+
+主要利用scipy计算两个句子向量中的各种度量，
+> 这里为了省事直接使用了doc2vec，可以快速标示句子向量
+
+- skew
+> 偏度（skewness），是统计数据分布偏斜方向和程度的度量，是统计数据分布非对称程度的数字特征。
+偏度(Skewness)亦称偏态、偏态系数。 
+- kurtosis
+
+峰度是描述总体中所有取值分布形态陡缓程度的统计量。
+
+句子相似度度量
+- doc_sims
+- cosine 余弦距离
+- cityblock 曼哈顿距离
+- jaccard
+ > 杰卡德距离(Jaccard Distance) 是用来衡量两个集合差异性的一种指标，它是杰卡德相似系数的补集，被定义为1减去Jaccard相似系数
+- canberra
+- euclidean 
+- minkowski
+- braycurtis
+
+
+## 参考资料
+- [3.1.6 峰度（Kurtosis）和偏度（Skewness）](https://blog.csdn.net/binbigdata/article/details/79897160)
--- features/gen_fea_embedding.py
+++ features/gen_fea_embedding.py
@@ -7,3 +7,92 @@
 @Software: PyCharm 
 @Description:
 """
+import pandas as pd
+import numpy as np
+import jieba
+from gensim.models.doc2vec import Doc2Vec, LabeledSentence
+from gensim.models.doc2vec import TaggedDocument
+from scipy.stats import skew, kurtosis
+from scipy.spatial.distance import cosine, cityblock, jaccard, canberra, euclidean, minkowski, braycurtis
+from tqdm import tqdm
+
+df_train = pd.read_csv('../data/input/atec_nlp_sim_train.csv',
+                       encoding='utf-8-sig', header=None, sep='\t')
+df_train.columns = ['line', 'q1', 'q2', 'label']
+
+df_train_add = pd.read_csv('../data/input/atec_nlp_sim_train_add.csv',
+                           encoding='utf-8-sig', header=None, sep='\t')
+df_train_add.columns = ['line', 'q1', 'q2', 'label']
+df_train = pd.concat([df_train, df_train_add], axis=0, sort=False)
+
+df_feat = pd.DataFrame()
+
+
+def tokenize(sequence):
+    return [word for word in jieba.cut(sequence)]
+
+
+
+df_train['qid1'] = ['qid1_' + str(i) for i in range(len(df_train.q1))]
+df_train['qid2'] = ['qid2_' + str(i) for i in range(len(df_train.q2))]
+qids = df_train.qid1.tolist() + df_train.qid2.tolist()
+
+
+def train_doc2vec():
+    print("training doc2vec...")
+    df_train.q1 = df_train.q1.apply(lambda x: tokenize(x))
+    df_train.q2 = df_train.q2.apply(lambda x: tokenize(x))
+    questions = df_train.q1.tolist() + df_train.q2.tolist()
+
+    tag_tokenized = [TaggedDocument(question, [id])
+                     for question, id in zip(questions, qids)]
+    model = Doc2Vec(size=300, min_count=1, epochs=200)
+    model.build_vocab(tag_tokenized)
+    model.train(tag_tokenized, total_examples=model.corpus_count, epochs=model.epochs)
+    model.save('tmp/word_doc2vec.model')
+
+
+# train_doc2vec()
+doc_sims = []
+cos_distance = []
+cityblock_distance = []
+jaccard_distance = []
+canberra_distance = []
+euclidean_distance = []
+minkowski_distance = []
+braycurtis_distance = []
+skew_q1vec = []
+skew_q2vec = []
+kur_q1vec = []
+kur_q2vec = []
+model_dm = Doc2Vec.load('tmp/word_doc2vec.model')
+
+for qid1, qid2 in tqdm(zip(df_train['qid1'], df_train['qid2'])):
+    doc_sims.append(model_dm.docvecs.similarity(qid1, qid2))  # 参数文档对应的id
+    cos_distance.append(cosine(model_dm.docvecs[qid1], model_dm.docvecs[qid2]))
+    cityblock_distance.append(cityblock(model_dm.docvecs[qid1], model_dm.docvecs[qid2]))
+    jaccard_distance.append(jaccard(model_dm.docvecs[qid1], model_dm.docvecs[qid2]))
+    canberra_distance.append(canberra(model_dm.docvecs[qid1], model_dm.docvecs[qid2]))
+    euclidean_distance.append(euclidean(model_dm.docvecs[qid1], model_dm.docvecs[qid2]))
+    minkowski_distance.append(minkowski(model_dm.docvecs[qid1], model_dm.docvecs[qid2]))
+    braycurtis_distance.append(braycurtis(model_dm.docvecs[qid1], model_dm.docvecs[qid2]))
+    skew_q1vec.append(skew(model_dm.docvecs[qid1]))
+    skew_q2vec.append(skew(model_dm.docvecs[qid2]))
+    kur_q1vec.append(skew(model_dm.docvecs[qid1]))
+    kur_q2vec.append(skew(model_dm.docvecs[qid2]))
+
+df_feat['doc_sim'] = doc_sims
+df_feat['cos_distance'] = cos_distance
+df_feat['cityblock_distance'] = cityblock_distance
+# df_feat['jaccard_distance'] = jaccard_distance
+df_feat['canberra_distance'] = canberra_distance
+df_feat['euclidean_distance'] = euclidean_distance
+df_feat['minkowski_distance'] = minkowski_distance
+df_feat['braycurtis_distance'] = braycurtis_distance
+df_feat['skew_q1vec'] = skew_q1vec
+df_feat['skew_q2vec'] = skew_q2vec
+df_feat['kur_q1vec'] = kur_q1vec
+df_feat['kur_q2vec'] = kur_q2vec
+
+
+df_feat.to_csv('subfeas/train_feature_embedding.csv',index=False)
